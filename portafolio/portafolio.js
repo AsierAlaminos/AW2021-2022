68a556930fed41f11f5b019d69f5d95cd5b01634
AsierAlaminos <91246701+AsierAlaminos@users.noreply.github.com>
2021-12-20 19:41:47 +0100

    Add files via upload

--- portafolio/portafolio.js
+++ portafolio/portafolio.js
@@ -1,17 +1,27 @@
 function cambiarmodo(){
     var web=document.body;
     var letracambio=document.getElementById("letra_cambio");
-    var checkbox=document.getElementById("ligth_dark")
+    var checkbox=document.getElementById("ligth_dark");
     web.classList.toggle("body_oscuro");
     if (checkbox.checked==true){
         letracambio.style.color="white";
         letracambio.innerHTML="Dark";
+        document.getElementById("texto_empresas_electroeco").style.color="white";
+        document.getElementById("texto_empresas_facebook").style.color="white";
+        document.getElementById("texto_empresas_youtube").style.color="white";
+        document.getElementById("texto_empresas_twitter").style.color="white";
+        document.getElementById("texto_empresas_twitch").style.color="white";
         document.getElementById("form_container").style.borderColor="black";
         document.getElementById("container_empresas").style.borderColor="white";
     }
     else{
         letracambio.style.color="black";
         letracambio.innerHTML="Light";
+        document.getElementById("texto_empresas_electroeco").style.color="black";
+        document.getElementById("texto_empresas_facebook").style.color="black";
+        document.getElementById("texto_empresas_youtube").style.color="black";
+        document.getElementById("texto_empresas_twitter").style.color="black";
+        document.getElementById("texto_empresas_twitch").style.color="black";
         document.getElementById("form_container").style.borderColor="white";
         document.getElementById("container_empresas").style.borderColor="black";
     }
@@ -38,3 +48,11 @@ var correo=function(){
     document.getElementById('numero_form').value="";
     document.getElementById('razon_form').value="";
 };
+
+function redes_sociales(){
+    if (document.getElementById("redes").style.display=="none"){
+        document.getElementById("redes").style.display="block";
+    }else{
+        document.getElementById("redes").style.display="none";
+    }
+}
